be6a6aafe39ef9c29c60a6b59aa4270a5eece424
lcassio <lcassio@users.noreply.github.com>
2016-10-04 14:18:22 -0300

    Atualizado com o "log" do endereço IP do servidor.

--- webserver-sadhi.js
+++ webserver-sadhi.js
@@ -1,5 +1,6 @@
 const http = require('http');
 const fs = require('fs');
+const os = require('os');
 
 const hostname = '127.0.0.1';
 const port = 3000;
@@ -9,9 +10,20 @@ const server = http.createServer((req, res) => {
   res.setHeader('Content-Type', 'text/html');
   fs.readFile('tela.html', 'utf8', (err, data)=> {
     if (err) throw err;
-    console.log(data);
     res.end(data);
   });
+  var interfaces = os.networkInterfaces();
+  var addresses = [];
+  for (var k in interfaces) {
+      for (var k2 in interfaces[k]) {
+          var address = interfaces[k][k2];
+          if (address.family === 'IPv4' && !address.internal) {
+              addresses.push(address.address);
+          }
+      }
+  }
+
+  console.log(addresses);
 });
 
 server.listen(port, hostname, () => {
